c1240937552f07b2bb11982f76d2211c7b1303dd
JimmyRamsamynaick <jimmyramsamynaick@gmail.com>
2026-01-09 22:20:19 +0100

    fix: prevent +say command crash on long messages by moving content to description

--- src/commands/utils/say.js
+++ src/commands/utils/say.js
@@ -13,7 +13,22 @@ module.exports = {
         if (!content) return message.channel.send({ embeds: [createEmbed(await t('say.usage', message.guild.id), '', 'info')] });
 
         message.delete().catch(() => {});
-        // message.channel.send(content);
-        await message.channel.send({ embeds: [createEmbed(content, '', 'info')] });
+        
+        let title = content;
+        let description = '';
+
+        // Discord Limit: Title max 256 chars
+        if (content.length > 256) {
+            const firstNewline = content.indexOf('\n');
+            if (firstNewline > -1 && firstNewline <= 256) {
+                title = content.substring(0, firstNewline);
+                description = content.substring(firstNewline + 1);
+            } else {
+                title = ' '; // Use space to keep the icon
+                description = content;
+            }
+        }
+
+        await message.channel.send({ embeds: [createEmbed(title, description, 'info')] });
     }
 };
